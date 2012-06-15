9e42bfdc589669bf64bc6bb7117d2c575c0a05d9
codeguesser <codeguesser@gmail.com>
2012-06-15 10:06:45 +0800

    Initial Commit

---
+++ testUploadFile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testUploadFile
+//
+//  Created by  on 2012/6/15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
